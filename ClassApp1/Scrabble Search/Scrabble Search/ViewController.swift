96c1d601096d5320cc1ac1539c00424a5ce399df
Kevina Wong <kevinawongw@gmail.com>
2022-01-27 16:22:59 -0700

    Scrabble

---
+++ ClassApp1/Scrabble Search/Scrabble Search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scrabble Search
+//
+//  Created by Kevina Wong on 1/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lab1/Lab1/Lab2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Lab1
+//
+//  Created by Kevina Wong on 1/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var HomeTab: UITabBarItem!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
