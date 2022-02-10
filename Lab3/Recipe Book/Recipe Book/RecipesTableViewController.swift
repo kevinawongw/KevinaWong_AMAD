9a61b578a129f15142ea28ef1699ee62266f04d8
Kevina Wong <kevinawongw@gmail.com>
2022-02-10 14:54:13 -0700

    bug fix

--- Lab3/Recipe Book/Recipe Book/RecipesTableViewController.swift
+++ Lab3/Recipe Book/Recipe Book/RecipesTableViewController.swift
@@ -11,77 +11,48 @@ class RecipesTableViewController: UITableViewController {
     
     var foodTypeList = [String]()
     var foodData = FoodDataLoader()
-    var foodList = [Food]()
     var allData = [FoodData]()
     let file = "recipes"
     var selectedCategory = 0
     var searchController = UISearchController()
     
-    @IBAction func unwindSegue (_ segue:UIStoryboardSegue){
-        
-        if segue.identifier == "doneSegue"{
-            if let source = segue.source as? AddRecipeViewController{
-                if source.addedRecipe.isEmpty == false{
-                    foodData.addFood(index: selectedCategory, newName: source.addedRecipe, newTime: source.addedTime)
-                    
-                    foodList.append(Food(foodName: source.addedRecipe, foodTime: source.addedTime, foodIngredients: [String](), foodImage: "cooking"))
-                    tableView.reloadData()
-                }
-            }
-        }
-    }
-    
     override func viewDidLoad() {
         
         super.viewDidLoad()
 
-
+        
         foodData.loadData(fileName: file)
         allData = foodData.getAllData()
         foodTypeList = foodData.getFoodTypes()
-        foodList = foodData.getFood(index: selectedCategory)
         
         let resultsController = SearchTableViewController()
         resultsController.allRecipes = allData
         searchController = UISearchController(searchResultsController: resultsController)
-        
-        resultsController.tableView?.delegate = self
-        
-        searchController.searchBar.placeholder = "Enter word to search"
+                
+        searchController.searchBar.placeholder = "Enter recipe to search"
         searchController.searchBar.sizeToFit()
         tableView.tableHeaderView = searchController.searchBar
         searchController.searchResultsUpdater = resultsController
             
-                
     }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "detailSegue" {
-            if let detailVC = segue.destination as? DetailViewController {
-                if let indexPath = tableView.indexPath(for: (sender as? UITableViewCell)!){
-                    detailVC.foodName = foodList[indexPath.row].foodName
-                        detailVC.foodIngredients = foodList[indexPath.row].foodIngredients
-                        detailVC.foodTime = foodList[indexPath.row].foodTime
-                        detailVC.foodImageName = foodList[indexPath.row].foodImage
-                }
-            }
-        }
-    }
-
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return foodList.count
+        return allData[selectedCategory].foods.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        foodList = foodData.getFood(index: selectedCategory)
+        
+        let recipesInSection = allData[selectedCategory].foods
+
         let cell = tableView.dequeueReusableCell(withIdentifier: "foodCell", for: indexPath)
         var cellConfig = cell.defaultContentConfiguration()
-        cellConfig.text = foodList[indexPath.row].foodName
+        
+        cellConfig.text = recipesInSection[indexPath.row].foodName
+        
         cellConfig.textProperties.font = UIFont(name: "FredokaOne-Regular", size: 12.0) ?? UIFont(name: "Helvetica", size: 20.0)!
         cellConfig.textProperties.color = UIColor.darkGray
         cell.contentConfiguration = cellConfig
@@ -94,18 +65,19 @@ class RecipesTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if searchController.searchBar.text?.isEmpty == false{
-            let story = UIStoryboard(name: "Main", bundle: nil)
-            let detailVC = story.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
-                detailVC.foodName = foodList[indexPath.row].foodName
-                detailVC.foodIngredients = foodList[indexPath.row].foodIngredients
-                detailVC.foodTime = foodList[indexPath.row].foodTime
-                detailVC.foodImageName = foodList[indexPath.row].foodImage
-            self.navigationController?.pushViewController(detailVC, animated: true)
-        }
-        print("running here")
-        tableView.deselectRow(at: indexPath, animated: true)
-        searchController.isActive = false
+        
+//        if searchController.searchBar.text?.isEmpty == false{
+//            let story = UIStoryboard(name: "Main", bundle: nil)
+//            let detailVC = story.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+//                detailVC.foodName = allData[selectedCategory].foods[indexPath.row].foodName
+//                detailVC.foodIngredients = allData[selectedCategory].foods[indexPath.row].foodIngredients
+//                detailVC.foodTime = allData[selectedCategory].foods[indexPath.row].foodTime
+//                detailVC.foodImageName = allData[selectedCategory].foods[indexPath.row].foodImage
+//            self.navigationController?.pushViewController(detailVC, animated: true)
+//        }
+//        print("running here")
+//        tableView.deselectRow(at: indexPath, animated: true)
+//        searchController.isActive = false
     }
 
 
@@ -136,7 +108,7 @@ class RecipesTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
            
-            foodList.remove(at: indexPath.row)
+            allData[selectedCategory].foods.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
             
             foodData.deleteFood(index: selectedCategory, foodIndex: indexPath.row)
@@ -163,14 +135,37 @@ class RecipesTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
+     
+     @IBAction func unwindSegue (_ segue:UIStoryboardSegue){
+         
+         if segue.identifier == "doneSegue"{
+             if let source = segue.source as? AddRecipeViewController{
+                 if source.addedRecipe.isEmpty == false{
+                     foodData.addFood(index: selectedCategory, newName: source.addedRecipe, newTime: source.addedTime)
+                     
+                     allData[selectedCategory].foods.append(Food(foodName: source.addedRecipe, foodTime: source.addedTime, foodIngredients: [String](), foodImage: "cooking"))
+                     tableView.reloadData()
+                 }
+             }
+         }
+     }
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "detailSegue" {
+            if let detailVC = segue.destination as? DetailViewController {
+                if let indexPath = tableView.indexPath(for: (sender as? UITableViewCell)!){
+                    
+                    detailVC.foodName = allData[selectedCategory].foods[indexPath.row].foodName
+                    detailVC.foodIngredients = allData[selectedCategory].foods[indexPath.row].foodIngredients
+                    detailVC.foodTime = allData[selectedCategory].foods[indexPath.row].foodTime
+                    detailVC.foodImageName = allData[selectedCategory].foods[indexPath.row].foodImage
+                }
+            }
+        }
     }
-    */
 
 }
--- Lab3/Recipe Book/Recipe Book/SearchTableViewController.swift
+++ Lab3/Recipe Book/Recipe Book/SearchTableViewController.swift
@@ -9,12 +9,9 @@ import UIKit
 
 class SearchTableViewController: UITableViewController, UISearchResultsUpdating {
     
-    
     var allRecipes = [FoodData]()
     var filteredFoodList = [Food]()
-//    var filteredRecipes = [String]()
-    var filteredFoodChoice: Food?
-    
+
     func updateSearchResults(for searchController: UISearchController) {
         
         let searchString = searchController.searchBar.text
@@ -41,16 +38,8 @@ class SearchTableViewController: UITableViewController, UISearchResultsUpdating 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.allowsSelection = true
-        tableView.delegate = self
-        tableView.allowsSelection = true
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "foodCell")
-        tableView.backgroundColor = UIColor(named: "myGreen")
-        // Uncomment the following line to preserve selection between presentations
-//         self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-         self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.delegate = self
     }
 
     // MARK: - Table view data source
@@ -72,60 +61,37 @@ class SearchTableViewController: UITableViewController, UISearchResultsUpdating 
         cellConfig.textProperties.font = UIFont(name: "FredokaOne-Regular", size: 12.0) ?? UIFont(name: "Helvetica", size: 20.0)!
         cellConfig.textProperties.color = UIColor.darkGray
         cell.contentConfiguration = cellConfig
-        
 
         return cell
     }
-
 
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "Search"
     }
     
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+        
+        
+        let story = UIStoryboard(name: "Main", bundle: nil)
+        let detailVC = story.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        detailVC.foodName = filteredFoodList[indexPath.row].foodName
+        detailVC.foodIngredients = filteredFoodList[indexPath.row].foodIngredients
+        detailVC.foodTime = filteredFoodList[indexPath.row].foodTime
+        detailVC.foodImageName = filteredFoodList[indexPath.row].foodImage
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+        if let selectServicesController = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController {
+            let navigationController = UINavigationController(rootViewController: selectServicesController)
+            
+            self.navigationController?.present(navigationController, animated: true, completion: {print("OK")})
+        }
+        
+        self.view.window!.rootViewController = detailVC
+        navigationController?.pushViewController(detailVC, animated: true)
+        print("running here")
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        }
+        
 }
